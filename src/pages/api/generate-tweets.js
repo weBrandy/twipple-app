04ae5884b23e6dc1f4b12229c4c891efab2b0b79
Andy Weber <andyweber@Macbookster.local>
2023-06-30 13:30:55 +0100

    Fixed broken syntax in tweet generation handler and verified eslint config

--- src/pages/api/generate-tweets.js
+++ src/pages/api/generate-tweets.js
@@ -8,5 +8,12 @@ export default async function generateTweetsHandler(req, res) {
 
   try {
     // Fetch the user input from the request body
-    const { contentPillars, characterOfContent, targetAudience, sentiment, 
-length, complexity } = req.body
+    const { contentPillars, engagementType, targetAudience, sentiment, length, complexity } = req.body;
+
+    // Further processing here...
+    
+  } catch (err) {
+    // Handle error here...
+    res.status(500).json({ error: err.message });
+  }
+}
